77d6de5c995b9fbf01ba103d522ccc0d5fdc04a1
Ahmd-Naser <ahmdnasr197@gmail.com>
2024-01-13 23:44:37 +0200

    Add project files.

---
+++ WebApplication6/About.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplication6.About" culture="auto" meta:resourcekey="PageResource1" uiculture="auto" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h2 id="title">
+            <asp:Button ID="EnglishButton" runat="server" Text="English" OnClick="LanguageButton_Click" CommandArgument="en" meta:resourcekey="EnglishButtonResource1" />
+            <asp:Button ID="SpanishButton" runat="server" Text="Arabic" OnClick="LanguageButton_Click" CommandArgument="ar" meta:resourcekey="SpanishButtonResource1" />
+
+            <%: Title %>.<asp:Label ID="Label1" runat="server" Text="Label" meta:resourcekey="Label1Resource1"></asp:Label>
+    </h2>
+        <h3>Your application description page.</h3>
+        <p>Use this area to provide additional information.</p>
+    </main>
+</asp:Content>
---
+++ WebApplication6/About.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set the initial language
+                SetLanguage("en");
+            }
+        }
+
+        protected void SetLanguage(string languageCode)
+        {
+            // Set the culture
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(languageCode);
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(languageCode);
+
+            // Update the page controls
+            Label1.Text = GetLocalResourceObject("Label1Resource1.Text").ToString();
+          
+        }
+
+        protected void LanguageButton_Click(object sender, EventArgs e)
+        {
+            // Handle the language switch event
+            string languageCode = ((Button)sender).CommandArgument.ToString();
+            SetLanguage(languageCode);
+        }
+
+    }
+}
